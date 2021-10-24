2c6cd5b0191a84ee8dda977066922c6ec85dc4e7
apomalyn <chretienxavier42@gmail.com>
2021-10-24 16:01:28 -0400

    Convert device uuid into byte array

--- src/bluetooth/characteristics/device_identity_characteristic.py
+++ src/bluetooth/characteristics/device_identity_characteristic.py
@@ -5,7 +5,15 @@ from pybleno import Characteristic, Descriptor
 class DeviceIdentityCharacteristic(Characteristic):
 
     def __init__(self, characteristic_config, device_uuid):
+        """
+
+        :param characteristic_config:
+        :type characteristic_config: dict
+        :param device_uuid:
+        :type device_uuid: str
+        """
         descriptors = []
+        self._device_uuid = device_uuid
 
         for descriptor in characteristic_config['descriptors']:
             descriptors.append(Descriptor({
@@ -17,5 +25,14 @@ class DeviceIdentityCharacteristic(Characteristic):
             'uuid': characteristic_config['uuid'],
             'properties': ['read'],
             'descriptors': descriptors,
-            'value': array.array('B', device_uuid)
+            'value': None
         })
+
+    def onReadRequest(self, offset, callback):
+        if offset:
+            callback(Characteristic.RESULT_ATTR_NOT_LONG, None)
+        else:
+            data = array.array('b')
+            data.frombytes(self._device_uuid.encode())
+
+            callback(Characteristic.RESULT_SUCCESS, data)
