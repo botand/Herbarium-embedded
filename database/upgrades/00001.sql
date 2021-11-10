c55149ba5bf1aedf408da7417fa91e9a5d2c68b5
Xavier Chrétien <chretienxavier42@gmail.com>
2021-11-10 11:38:33 -0500

    Add the SQL script to initialize the database.

---
+++ database/upgrades/00001.sql
@@ -0,0 +1,39 @@
+-- Creation date: 10 nov 2021
+
+create table if not exists plant
+(
+    uuid                        TEXT
+        primary key,
+    position                    INTEGER not null,
+    moisture_goal               REAL    default 80.0 not null,
+    light_exposure_min_duration REAL    default 14.0 not null,
+    planted_at                  INTEGER default (time('now')) not null,
+    check (light_exposure_min_duration >= 0.0 AND light_exposure_min_duration < 24.0),
+    check (moisture_goal >= 0.0 AND moisture_goal < 100.0),
+    check (position >= 0)
+);
+
+create table if not exists actions
+(
+    type        TEXT    not null,
+    timestamp   INTEGER default (time('now')) not null,
+    status      BOOLEAN not null,
+    plant_uuid  TEXT
+        references plant
+            on delete cascade,
+    transmitted BOOLEAN default 0 not null,
+    check (type in ('V', 'L'))
+);
+
+create table if not exists sensors_data
+(
+    type        TEXT not null,
+    timestamp   INTEGER default (time('now')) not null,
+    value       REAL not null,
+    plant_uuid  TEXT
+        references plant,
+    transmitted BOOLEAN default 0 not null,
+    check (type in ('M', 'L', 'T'))
+);
+
+
