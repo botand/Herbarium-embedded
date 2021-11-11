0a3869221575625ebdd2bca5bb8861f717766a47
Charles Chaum <c.chaumi@gmail.com>
2021-11-11 17:52:52 -0500

    Valve, Pompe et LED Strip, ils débarquent dans les bacs ! (#6)
    On a tout vérifié et Wallah ca marche !

---
+++ src/services/lightning_led_strip_service.py
@@ -0,0 +1,71 @@
+"""Service to interact with the LED Lightning"""
+from neopixel import NeoPixel
+from src.utils import pin_number_to_digital_gpio, led_utils, get_logger
+
+_SERVICE_TAG = "services.LightningLedStripService"
+_LED_COUNT_CONFIG_KEY = "led_count"
+_LED_PIN_CONFIG_KEY = "gpio_data_in"
+_LED_BY_TILE_KEY = "led_by_tile"
+
+
+class LightningLedService:
+    """
+    Service to interact with the LED Lightning
+    """
+
+    _logger = get_logger(_SERVICE_TAG)
+
+    def __init__(self, config):
+        """
+        :param config: configuration file to use.
+        :type config : dict of str
+        """
+        self._led_count = config[_LED_COUNT_CONFIG_KEY]
+        self._led_by_tile = config[_LED_BY_TILE_KEY]
+        self._strip = NeoPixel(
+            pin_number_to_digital_gpio(config[_LED_PIN_CONFIG_KEY]),
+            self._led_count,
+            auto_write=False,
+        )
+
+    def _update_segment(self, tile_nb, color, brightness):
+        """
+        Update one selected segment by it number.
+        :param tile_nb: number of the tile [0-15]
+        :param color: selected color in led_utils.py
+        :param brightness: [0.0-1.0]
+        """
+        for i in range(
+            tile_nb * self._led_by_tile - self._led_by_tile, tile_nb * self._led_by_tile
+        ):
+            self._strip[i] = (
+                round(color[0] * brightness),
+                round(color[1] * brightness),
+                round(color[2] * brightness),
+            )
+        self._strip.show()
+
+    def turn_on(self, tile_nb):
+        """
+        Turn ON tile selected lightning
+        :param tile_nb: tile number [0-15]
+        """
+        self._update_segment(tile_nb, led_utils.COLOR_WHITE, 1)
+        self._logger.debug(f"{_SERVICE_TAG} Turn ON Tile {tile_nb} Lightning")
+
+    def turn_off(self, tile_nb):
+        """
+        Turn OFF tile selected lightning
+        :param tile_nb: tile number [0-15]
+        """
+        self._update_segment(tile_nb, led_utils.COLOR_WHITE, 0)
+        self._logger.debug(f"{_SERVICE_TAG} Turn OFF Tile {tile_nb} Lightning")
+
+    def turn_off_all(self):
+        """
+        Turn OFF ALL tile lightning
+        """
+        for i in range(self._led_count):
+            self._strip[i] = led_utils.COLOR_BLACK
+        self._strip.show()
+        self._logger.debug(f"{_SERVICE_TAG} Turn OFF All Tiles Lightning")
---
+++ src/services/valve_service.py
@@ -0,0 +1,179 @@
+"""Service to interact with the valves"""
+from RPi import GPIO
+from src.utils import time_in_millisecond, get_logger
+
+_SERVICE_TAG = "service.ValveService"
+_VALVE_SELECTOR_PIN_S0 = "gpio_selector_pin_S0"
+_VALVE_SELECTOR_PIN_S1 = "gpio_selector_pin_S1"
+_VALVE_SELECTOR_PIN_S2 = "gpio_selector_pin_S2"
+_VALVE_SELECTOR_PIN_S3 = "gpio_selector_pin_S3"
+_VALVE_PIN_CONFIG_KEY = "gpio_position_out"
+_PWM_FREQ = "pwm_freq"
+_VALVE_POSITION_OFF = "position_off"
+_VALVE_POSITION_ON = "position_on"
+_VALVE_OPENING_TIME = "opening_time"
+_VALVE_CLOSING_TIME = "closing_time"
+
+
+class ValveService:
+    """
+    Service to interact with the valves
+    """
+
+    _logger = get_logger(_SERVICE_TAG)
+
+    def __init__(self, config):
+        """
+        :param config: configuration to use.
+        :type config : dict of str
+        """
+
+        self._valve_pin = config[_VALVE_PIN_CONFIG_KEY]
+        self._valve_s0 = config[_VALVE_SELECTOR_PIN_S0]
+        self._valve_s1 = config[_VALVE_SELECTOR_PIN_S1]
+        self._valve_s2 = config[_VALVE_SELECTOR_PIN_S2]
+        self._valve_s3 = config[_VALVE_SELECTOR_PIN_S3]
+        self._pwm_freq = config[_PWM_FREQ]
+        self._position = {
+            "open": config[_VALVE_POSITION_ON],
+            "close": config[_VALVE_POSITION_OFF],
+        }
+        self._timing = {
+            "open": config[_VALVE_OPENING_TIME],
+            "close": config[_VALVE_CLOSING_TIME],
+        }
+
+        # GPIO Assignation and configuration
+        GPIO.setup(self._valve_s0, GPIO.OUT)
+        GPIO.setup(self._valve_s1, GPIO.OUT)
+        GPIO.setup(self._valve_s2, GPIO.OUT)
+        GPIO.setup(self._valve_s3, GPIO.OUT)
+
+        GPIO.setup(self._valve_pin, GPIO.OUT)
+        self._valve = GPIO.PWM(self._valve_pin, self._pwm_freq)
+        self._valve.start(0)
+
+        # Valve position mechanism
+        self._valve_state = (
+            []
+        )  # actual position for all valves, will be fulled by initializing process
+        self._asked_valve_state = []  # wait line of tuple (addr, asked_pos)
+        self._is_moving = False
+        self._previous_time = time_in_millisecond()
+
+        # Initiate valves
+        self._logger.debug(
+            f"{_SERVICE_TAG} Valve Initialisation - Closing all the vales"
+        )
+        self.close_all()
+        for i in range(16):
+            self._valve_state.append("open")
+        while self._valve_state[15] != "close":
+            self.update()
+        self._logger.debug(
+            f"{_SERVICE_TAG} Valve Initialisation - Done !"
+        )
+
+    def _select_addr(self, valve_nb):
+        """
+        :param valve_nb: decimal selected valve [0-15]
+        :type valve_nb: int
+        Convert the decimal value into binary and update the GPIO selectors
+        """
+        bin_valve_nb = "{0:b}".format(valve_nb)
+
+        # number of 0 correction, all number must be on 4 digit
+        zero = "0"
+        for i in range(4 - len(bin_valve_nb)):
+            bin_valve_nb = zero + bin_valve_nb
+
+        if bin_valve_nb[0] == "1":
+            GPIO.output(self._valve_s3, GPIO.HIGH)
+        else:
+            GPIO.output(self._valve_s3, GPIO.LOW)
+
+        if bin_valve_nb[1] == "1":
+            GPIO.output(self._valve_s2, GPIO.HIGH)
+        else:
+            GPIO.output(self._valve_s2, GPIO.LOW)
+
+        if bin_valve_nb[2] == "1":
+            GPIO.output(self._valve_s1, GPIO.HIGH)
+        else:
+            GPIO.output(self._valve_s1, GPIO.LOW)
+
+        if bin_valve_nb[3] == "1":
+            GPIO.output(self._valve_s0, GPIO.HIGH)
+        else:
+            GPIO.output(self._valve_s0, GPIO.LOW)
+
+    def close(self, tile_nb):
+        """
+        Close selected Valve
+        :param tile_nb : tile number [0-15]
+        :type tile_nb: int
+        """
+        self._asked_valve_state.append(
+            (tile_nb, "close")
+        )  # Add close request at tail
+
+    def close_all(self):
+        """
+        Close all valves
+        """
+        for i in range(16):
+            self.close(i)
+
+    def open(self, tile_nb):
+        """
+        Open Selected Valves
+        :param tile_nb : tile number [0-15]
+        """
+        self._asked_valve_state.append(
+            (tile_nb, "open")
+        )  # Add open request at tail
+
+    def update(self):
+        """
+        Update the valves status and apply PWM control
+        """
+        # is buffer is empty just pass
+        if len(self._asked_valve_state) == 0:
+            return
+
+        asked_addr = int(self._asked_valve_state[0][0])
+        asked_state = self._asked_valve_state[0][1]
+
+        # if the valve is already in the asked position just pass too
+        if asked_state == self._valve_state[asked_addr]:
+            self._asked_valve_state.pop(0)  # Remove the asked position
+            self._logger.debug(
+                f"{_SERVICE_TAG} Valve {asked_addr} already in position {asked_state}"
+            )
+            return
+
+        # if we need to modify the valve position, check time and move !
+        actual_time = time_in_millisecond()
+
+        # if it's the first update for the actual movement request, then update previous
+        if not self._is_moving:
+            self._is_moving = True
+            self._previous_time = time_in_millisecond()
+
+        # So if the movement is incomplete
+        if actual_time - self._previous_time < self._timing[asked_state]:
+            self._select_addr(asked_addr)  # valve selection
+            self._valve.ChangeDutyCycle(
+                self._position[asked_state]
+            )  # update valve movement
+
+        # if the movement is finished
+        else:
+            self._select_addr(asked_addr)  # valve selection
+            self._valve.ChangeDutyCycle(0)  # stop PWM
+            self._valve_state[asked_addr] = asked_state  # Update valve movement
+            self._asked_valve_state.pop(0)  # Remove the asked position
+            self._is_moving = False
+            self._logger.debug(
+                f"{_SERVICE_TAG} Valve {asked_addr} moved to position {asked_state}"
+            )
