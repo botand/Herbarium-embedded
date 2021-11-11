8706ce09e09025675803855013179c234fd3d392
Charles Chaum <c.chaumi@gmail.com>
2021-11-11 16:09:57 -0500

    Validation Mux Valve

--- src/__main__.py
+++ src/__main__.py
@@ -66,7 +66,7 @@ def main():
 
 
 
-            if time_in_millisecond() - prev > 1000:
+            if time_in_millisecond() - prev > 3000:
                 prev = time_in_millisecond()
             #
             #     if tile > 16:
