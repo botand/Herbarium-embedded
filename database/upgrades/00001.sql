c64046957e71c7d714e2b5fcb9396199a9a2986a
Xavier Chrétien <chretienxavier42@gmail.com>
2021-11-16 09:56:41 -0500

    Update SQL scripts to match the latest version of the DB.

--- database/upgrades/00001.sql
+++ database/upgrades/00001.sql
@@ -8,6 +8,7 @@ create table if not exists plant
     moisture_goal               REAL    default 80.0 not null,
     light_exposure_min_duration REAL    default 14.0 not null,
     planted_at                  INTEGER default (time('now')) not null,
+    transmitted                 BOOLEAN default 0 not null,
     check (light_exposure_min_duration >= 0.0 AND light_exposure_min_duration < 24.0),
     check (moisture_goal >= 0.0 AND moisture_goal < 100.0),
     check (position >= 0)
@@ -22,7 +23,7 @@ create table if not exists actions
         references plant
             on delete cascade,
     transmitted BOOLEAN default 0 not null,
-    check (type in ('V', 'L'))
+    check (type in ('V', 'L', 'P'))
 );
 
 create table if not exists sensors_data
