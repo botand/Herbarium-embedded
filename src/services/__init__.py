d39a1c88f8dcc69e6cb3f20748dffb5647e95168
Charles Chaum <c.chaumi@gmail.com>
2021-12-02 18:11:48 -0500

    Feature/analog devices (#10)
    * Validation Mux ADC Plant hygrometry
    
    * Validation Mux ADC Plant hygrometry
    
    * Validation Mux ADC Plant hygrometry
    
    * Validation Mux ADC Plant hygrometry
    
    * Validation Mux ADC Plant hygrometry
    
    * Validation Mux ADC Plant hygrometry
    
    * ADC Service Test on Proto
    
    * ADC Service Test on Proto
    
    * ADC Service Test on Proto
    
    * ADC Service Test on Proto
    
    * ADC Service Test on Proto
    
    * ADC Service Test on Proto
    
    * ADC Service Test on Proto
    
    * ADC Service Test on Proto
    
    * Motherboard Test
    
    * Motherboard Test
    
    * Motherboard Test
    
    * Motherboard Test
    
    * Motherboard Test
    
    * Motherboard Test
    
    * Motherboard Test
    
    * Motherboard Test
    
    * Motherboard Test
    
    * ADC Service - Conversion de la lecutre de la luminosité ambiante en pourcentage
    
    * ADC Service - Conversion de la lecutre de la luminosité ambiante en pourcentage
    
    * Finalisation du prototype
    
    * Finalisation du prototype
    
    * Finalisation du prototype
    
    * Finalisation du prototype
    
    * Finalisation du prototype
    
    * Analog_Device - Caractérisation Water Level
    
    * ajout de la conversion de la mesure du niveau d'eau dans le réservoir.
    
    * Update adc_service.py
    
    debug logging ADC values
    
    * test main
    
    * Caracterisatio Hygro des Pots
    
    * Caractrisation hygro pot
    
    * debugcaracterisation
    
    * debug
    
    * debug
    
    * debug
    
    * final tests
    
    * Update revision number
    
    * Update config.yaml
    
    Co-authored-by: Xavier Chrétien <chretienxavier42@gmail.com>
    
    * Update src/services/adc_service.py
    
    Co-authored-by: Xavier Chrétien <chretienxavier42@gmail.com>
    
    * Update src/services/valve_service.py
    
    Co-authored-by: Xavier Chrétien <chretienxavier42@gmail.com>
    
    Co-authored-by: Xavier Chrétien <chretienxavier42@gmail.com>

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -5,4 +5,5 @@ from .lightning_led_strip_service import LightningLedService
 from .valve_service import ValveService
 from .pump_service import PumpService
 from .internet_connection_service import InternetConnectionService
+from .adc_service import ADCService
 from .database_service import DatabaseService
