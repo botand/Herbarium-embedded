6340d6595cb46c54e49252a36ab5348b3c1e5a14
Charles Chaum <c.chaumi@gmail.com>
2021-12-06 11:40:02 -0500

    Avancement de la logiicque de régulation de l'hygrométrie

--- src/controllers/hygrometry_regulation_controller.py
+++ src/controllers/hygrometry_regulation_controller.py
@@ -14,8 +14,10 @@ _CONTROLLER_TAG = "controllers.HygromertyRegulationController"
 
 
 # For water shot Attribution
+_DELTA_DETECTION = "delta_detection"
 _SHOT_DURATION = "shot_duration"
 _PUMP_SPEED = "pump_speed"
+_MAX_SAMPLE_REGULATION = "max_sample_before_regulation"
 
 
 class HygrometryRegulationController:
@@ -46,7 +48,9 @@ class HygrometryRegulationController:
         self._last_read = []
         self._average = []
         self._nb_sample = []
-    
+        self._delta_detection = config[_DELTA_DETECTION]
+        self._max_sample_regulation = config[_MAX_SAMPLE_REGULATION]
+
         # water Shot
         self._shot_duration = config[_SHOT_DURATION]
         self._pump_speed = config[_PUMP_SPEED]
@@ -57,13 +61,39 @@ class HygrometryRegulationController:
     def hygrometric_update(plants):
         for plant in plants: 
 
-            hygro_val = ADCService.instance().get_plant_hygrometry_value(plant.position)
-            cummulative = self._cummulative(plant.position)
-            last_read = self._last_read(plant.position)
-            average = self._average(plant.position)
+            hygro_val = ADCService.instance().get_plant_hygrometry_value(plant.position())
+            cummulative = self._cummulative(plant.position())
+            last_read = self._last_read(plant.position())
+            average = self._average(plant.position())
+            nb_sample = self._nb_sample(plant.position())
 
-            if hygro_val != average && _hygro_val == last_read  # TODO Ajouter la tolérence parce que la ca ne marchera pas
-                
+            # Si on a détecté une diférence d'hygrométrie spontannée on conjuge avec la dernière mesure 
+            # pour confirmer ce changement et détecter l'ajout ou le retrait d'un pot.
+            if ((hygro_val <= (average - self._delta_detection)) || (hygro_val >= (average + self._delta_detection))) && 
+            ((hygro_val >= (last_read - self._delta_detection)) && (hygro_val <= (last_read + self._delta_detection))):
+
+                # If it was an adding
+                if hygro_val >= (average + self._delta_detection)
+                    # TODO : aouter dans la dB
+                else
+                    # TODO : Retirer de la DB
+
+                # Redo the cummulatiive for a new average value
+                cummulative = last_read
+                nb_sample = 1
+
+            cummulative += hygro_val
+            nb_sample += 1
+            last_read = hygro_val
+
+            # Hyometric reglation at every MAX SAMPLE BEFORE REGULATION acquisiition cycle
+            if nb_sample == self._max_sample_regulation:
+                average = cummulative / nb_sample
+                if plant.moisture_goal() != None
+                    # TODO Send DB
+                    # TODO Regulatio procedure
+                cummulative = 0
+                nb_sample = 0              
 
         
 
