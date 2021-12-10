1f47394ec53f43028ec2ae97fd690850d31e6b9e
apomalyn <chretienxavier42@gmail.com>
2021-12-10 13:02:32 -0500

    Little fixes

--- src/controllers/hygrometry_regulation_controller.py
+++ src/controllers/hygrometry_regulation_controller.py
@@ -1,5 +1,4 @@
 """HygrometryRegulationController"""
-from src.models import plant
 from src.services import (
     ADCService,
     DatabaseService,
@@ -8,13 +7,13 @@ from src.services import (
 )
 from src.utils.configuration import config
 from src.utils import (
-    get_logger, 
+    get_logger,
     time_in_millisecond,
 )
 from src.utils.sql_queries import (
     INSERT_MOISTURE_LEVEL_FOR_PLANT,
     INSERT_NEW_PLANT,
-    REMOVE_PLANT
+    REMOVE_PLANT, INSERT_VALVE_ORDER, INSERT_PUMP_ORDER
 )
 
 _CONTROLLER_TAG = "controllers.HygromertyRegulationController"
@@ -30,34 +29,26 @@ _MAX_SAMPLE_REGULATION = "max_sample_before_regulation"
 _SHOT_DURATION = "shot_duration"
 _PUMP_SPEED = "pump_speed"
 
+
 class HygrometryRegulationController:
     """Controller That manage the hygrometry Regulation"""
 
-    __instance = None
     _logger = get_logger(_CONTROLLER_TAG)
-    _adc_instance = ADCService.instance()
-    _db_instance = DatabaseService.instance()
+    _adc_service = ADCService.instance()
+    _valve_service = ValveService.instance()
+    _pump_service = PumpService.instance()
+    _db_service = DatabaseService.instance()
 
-    @staticmethod
-    def instance():
-        """
-        Get the service
-        :rtype: HygrometryRegulationController
-        """
-        if HygrometryRegulationController.__instance is None:
-            HygrometryRegulationController.__instance = HygrometryRegulationController()
-        return HygrometryRegulationController.__instance
-    
     def __init__(self):
         """Initialize the Controller"""
 
         hygro_config = config[_CONFIG_TAG]
 
         # Hygrometry regulation, one value per plant position
-        self._cummulative = [0.0]*16
-        self._last_read = [0.0]*16
-        self._average = [0.0]*16
-        self._nb_sample = [0]*16
+        self._cummulative = [0.0] * 16
+        self._last_read = [0.0] * 16
+        self._average = [0.0] * 16
+        self._nb_sample = [0] * 16
         self._interval_update = hygro_config[_INTERVAL_UPDATE]
         self._delta_detection = hygro_config[_DELTA_DETECTION]
         self._max_sample_regulation = hygro_config[_MAX_SAMPLE_REGULATION]
@@ -67,16 +58,17 @@ class HygrometryRegulationController:
         self._shot_duration = hygro_config[_SHOT_DURATION]
         self._pump_speed = hygro_config[_PUMP_SPEED]
         self._shot_query_queue = []  # Contains plant position line.
-        self._query_status = False # False:wating, True:In progress
+        self._query_status = False  # False:wating, True:In progress
         self._previous_shot_time = 0
 
         # Initialisation
         for i in range(config[_PLANT_COUNT]):
-            hygro_value = _adc_instance.get_plant_hygrometry_value(i)
+            hygro_value = self._adc_service.get_plant_hygrometry_value(i)
             self._cummulative[i] = hygro_value
-            self._last_read[i] = hygro_value 
+            self._last_read[i] = hygro_value
             self._average[i] = hygro_value
             self._nb_sample[i] = 1
+        self._logger.debug("initialized")
 
     def update(self, plants):
         """
@@ -85,9 +77,9 @@ class HygrometryRegulationController:
         - Check for Hygrometry
         - Give some water shots for hygrometry regulation
         :param plants: plant list - 16 elements by ASC position
-        :ptype plants: list
+        :type plants: list of Plant
         """
-        self._shot_update()
+        self._shot_update(plants)
 
         if time_in_millisecond() - self._previous_read_time > self._interval_update:
             self._hygrometric_update(plants)
@@ -96,80 +88,79 @@ class HygrometryRegulationController:
     def _hygrometric_update(self, plants):
         """
         Check for the added or removed plants and ask for hygrometric regulation
-        :param plants: plant list - 16 elements by ASC position but it doesn't matter
-        :ptype plants: list
+        :param plants: plant list - 16 elements by ASC position, but it doesn't matter
+        :type plants: list of Plant
         """
-        for plant in plants: 
-            i = plant.position()
-            hygro_val = _adc_instance.get_plant_hygrometry_value(i)
+        for plant in plants:
+            i = plant.position
+            hygro_val = self._adc_service.get_plant_hygrometry_value(i)
 
             # Si on a détecté une diférence d'hygrométrie spontannée on conjuge avec la dernière mesure 
             # pour confirmer ce changement et détecter l'ajout ou le retrait d'un pot.
             # if hygro Val != avergae +/- delta AND hygro_val == last_read +/- delta
             difference_avg = hygro_val - self._average[i]
             difference_last_read = hygro_val - self._last_read[i]
-            if ((difference_avg <= -self._delta_detection) || (difference_avg >= self._delta_detection)) && 
-            ((difference_last_read >= -self._delta_detection) && (difference_last_read <= self._delta_detection)):
+            if ((difference_avg <= -self._delta_detection) or (difference_avg >= self._delta_detection)) and (
+                    (difference_last_read >= -self._delta_detection) and (
+                    difference_last_read <= self._delta_detection)):
 
-               # If it was an adding
+                # If it was an adding
                 if hygro_val >= (self._average[i] + self._delta_detection):
+                    # Add to DB
+                    self._db_service.execute(INSERT_NEW_PLANT, [i])
+                else:
+                    # Remove from DB
+                    self._db_service.execute(REMOVE_PLANT, [plants[i].uuid, i])
 
-                    # Ajouter dans la dB
-                    _db_instance.execute(INSERT_NEW_PLANT, i)
-                else
-                    # Retirer de la DB
-                    _db_instance.execute(REMOVE_PLANT, plants(i).uuid(), i)
+                # Redo the cumulative for a new average value
+                self._cummulative[i] = self._last_read[i]
+                self._nb_sample[i] = 1
 
-                # Redo the cummulative for a new average value
-                self._cummulative(i) = self._last_read(i)
-                self._nb_sample(i) = 1
+            self._cummulative[i] += hygro_val
+            self._nb_sample[i] += 1
+            self._last_read[i] = hygro_val
 
-            self._cummulative(i) += hygro_val
-            self._nb_sample(i) += 1
-            self._last_read(i) = hygro_val
-
-            # Hyometric reglation at every MAX SAMPLE BEFORE REGULATION acquisiition cycle
+            # Hygrometric regulation at every MAX SAMPLE BEFORE REGULATION acquisition cycle
             if self._nb_sample[i] == self._max_sample_regulation:
                 self._average[i] = self._cummulative[i] / self._nb_sample[i]
-                if plant.moisture_goal != None
-                    # Dataase Communication
-                    _db_instance.execute(INSERT_MOISTURE_LEVEL_FOR_PLANT, self._average[i], plants[i].uuid)
+                if plant.moisture_goal is not None:
+                    # Database Communication
+                    self._db_service.execute(INSERT_MOISTURE_LEVEL_FOR_PLANT, [self._average[i], plants[i].uuid])
                     # Regulation
-                    if self.average(i) < plat.moisture_goal()
+                    if self._average[i] < plant.moisture_goal:
                         self._query_shot(i)
-                self._cummulative(i) = 0
-                self._nb_sample(i) = 0              
-
+                self._cummulative[i] = 0
+                self._nb_sample[i] = 0
 
     def _query_shot(self, plant_position):
         """
         Add a shot query to the line.
         :param plant_position: plant position [0-15]
-        :ptype plant_position: int
+        :type plant_position: int
         """
         self._shot_query_queue.append(plant_position)
-        self._logger.debug(f"{_CONTROLLER_TAG} Shot Plannified for plant {plant_position}")
+        self._logger.debug("Shot planned for plant %d", plant_position)
 
-    def _shot_update(self):
+    def _shot_update(self, plants):
         """
         Execute the shot query line. Each shot is done one by one.
         Must be executed as quickly as possible.
+        :param plants: plant list - 16 elements by ASC position, but it doesn't matter
+        :type plants: list of Plant
         """
         if len(self._shot_query_queue) > 0:
-            if !self._query_status:
+            if not self._query_status:
                 self._previous_shot_time = time_in_millisecond()
-                _db_instance.execute(INSERT_VALVE_ORDER, "open", plants(self._shot_query_queue[0]).uuid())
-                ValveService.instance().open(self._shot_query_queue[0])
-                _db_instance.execute(INSERT_PUMP_ORDER, self._pump_seed)
-                PumpService.instance().set_speed(self._pump_seed)
+                self._db_service.execute(INSERT_VALVE_ORDER, [1, plants[self._shot_query_queue[0]].uuid])
+                self._valve_service.open(self._shot_query_queue[0])
+                self._db_service.execute(INSERT_PUMP_ORDER, [1])
+                self._pump_service.set_speed(self._pump_speed)
                 self._query_status = True
-            else:
-                if time_in_millisecond() - self._previous_shot_time > self._shot_duration:
-
-                    _db_instance.execute(INSERT_VALVE_ORDER, "close", plants(self._shot_query_queue[0]).uuid)
-                    ValveService.instance().close(self._shot_query_queue[0])
-                    _db_instance.execute(INSERT_PUMP_ORDER, 0)
-                    PumpService.instance().stop()
-                    self._query_status = False
-                    self._logger.debug(f"{_CONTROLLER_TAG} Shot Done for plant {self._shot_query_queue[0]}")
-                    self._shot_query_queue.pop(0)
+            elif (time_in_millisecond() - self._previous_shot_time) > self._shot_duration:
+                self._db_service.execute(INSERT_VALVE_ORDER, [0, plants[self._shot_query_queue[0]].uuid])
+                self._valve_service.close(self._shot_query_queue[0])
+                self._db_service.execute(INSERT_PUMP_ORDER, [0])
+                self._pump_service.stop()
+                self._query_status = False
+                self._logger.debug(f"{_CONTROLLER_TAG} Shot Done for plant {self._shot_query_queue[0]}")
+                self._shot_query_queue.pop(0)
--- src/controllers/luminosity_regulation_controller.py
+++ src/controllers/luminosity_regulation_controller.py
@@ -1,16 +1,13 @@
-"""Controller that manage the lumnosity regulation"""
-from src.models import plant
+"""Controller that manage the luminosity regulation"""
 from datetime import datetime
-
 from src.services import (
     ADCService,
     DatabaseService,
     LightningLedService
 )
-
 from src.utils.configuration import config
 from src.utils import (
-    get_logger, 
+    get_logger,
     time_in_millisecond,
 )
 
@@ -19,79 +16,70 @@ from src.utils.sql_queries import INSERT_LIGHT_STRIP_ORDER
 _CONTROLLER_TAG = "controllers.LuminosityRegulationController"
 _CONFIG_TAG = "luminosity"
 _PLANT_COUNT = "plant_count"
-_TZ_OFFSET = "time_zone_offfset"
+_TZ_OFFSET = "time_zone_offset"
 _TIME_RANGE_CENTER = "time_range_center"
 
-class LuminosityRegulationController:
-    """Controller that manage the lumnosity regulation"""
 
-    __instance = None
+class LuminosityRegulationController:
+    """Controller that manage the luminosity regulation"""
+
     _logger = get_logger(_CONTROLLER_TAG)
     _adc_instance = ADCService.instance()
     _db_instance = DatabaseService.instance()
     _led_instance = LightningLedService.instance()
 
-    @staticmethod
-    def instance():
-        """
-        Get the service
-        :rtype: LuminosityRegulationController
-        """
-        if LuminosityRegulationController.__instance is None:
-            LuminosityRegulationController.__instance = LuminosityRegulationController()
-        return LuminosityRegulationController.__instance
-
-    def _init_(self):
+    def __init__(self):
         """Initialize the Controller"""
         lum_config = config[_CONFIG_TAG]
-        time_zone_offset = config[_TZ_OFFSET]
+        self._time_zone_offset = config[_TZ_OFFSET]
 
         self._update_time = lum_config["interval_update"]
-        self._time_range_cennter = lum_config["_TIME_RANGE_CENTER"]
+        self._time_range_center = lum_config["_TIME_RANGE_CENTER"]
         self._previous_time = 0
         self.time = 0
+        self._logger.debug("initialized")
 
-
-    def update(self,plants):
+    def update(self, plants):
         """
         Execute the updates:
-        for each plant check if it has the required lignthing time.
+        for each plant check if it has the required lightning time.
         :param plants: plant list - 16 elements by ASC position
-        :ptype plants: list
+        :type plants: list
         """
-        if (time_in_millisecond() - self._previous_time) > self._update_time
-            self._previous_time = time_in_millisecond
+        if (time_in_millisecond() - self._previous_time) > self._update_time:
+            self._previous_time = time_in_millisecond()
 
             # What time is it ?
-            d = datetime.utcnow()
-            self.hour = (d.hour - self.time_zone_offset) + d.min/6
+            now = datetime.utcnow()
+            hour = (now.hour - self._time_zone_offset) + now.min / 6
 
             # Regulation for plants
             for plant in plants:
-
-                if _is_on(plant, hour):
-                    _led_instance.turn_on(plant.position, -_adc_instance.get_ambient_luminosity_value)
-                    self._logger.debug(f"{_CONTROLLER_TAG} Turn ON Tile {plant_position} Ligthning ({-_adc_instance.get_ambient_luminosity_value} %)")
+                if self._is_on(plant, hour):
+                    self._led_instance.turn_on(plant.position, -self._adc_instance.get_ambient_luminosity_value())
+                    self._logger.debug(
+                        "Turn ON Tile %d Lightning (%d %%)",
+                        plant.position, -self._adc_instance.get_ambient_luminosity_value())
                 else:
-                    _led_instance.turn_off(plant.position)
-                    self._logger.debug(f"{_CONTROLLER_TAG} Turn OFF Tile {plant_position} Ligthning")
+                    self._led_instance.turn_off(plant.position)
+                    self._logger.debug("Turn OFF Tile %d Lightning", plant.position)
 
-
-    def _is_on(self, plant):
+    def _is_on(self, plant, hour):
         """
-        detect if we need to turn on the lighning
-        :param plants: plant list - 16 elements by ASC position
-        :ptype plants: list
+        Detect if we need to turn on the lightning
+        :param plant: plant list - 16 elements by ASC position
+        :type plant: Plant
+        :return true if we need to turn on the lights
+        :rtype bool
         """
 
-        # Distribute the exposure time arround 14h to create the time range
-        time_range = (self._time_range_cennter-plant.light_exposure_min_duration/2, 
-        self._time_range_cennter+plant.light_exposure_min_duration/2)
+        # Distribute the exposure time around 14h to create the time range
+        time_range = (self._time_range_center - plant.light_exposure_min_duration / 2,
+                      self._time_range_center + plant.light_exposure_min_duration / 2)
 
-        # if we are in the time range, turn on the lightining.
-        if self.hour >= time_range[0] && self.hour <= time_range[1]:
-            _db_instance.execute(INSERT_LIGHT_STRIP_ORDER, "on", plants(i).uuid())
+        # if we are in the time range, turn on the lighting.
+        if time_range[0] <= hour <= time_range[1]:
+            self._db_instance.execute(INSERT_LIGHT_STRIP_ORDER, [1, plant.uuid])
             return True
-        else:
-            _db_instance.execute(INSERT_LIGHT_STRIP_ORDER, "off", plants(i).uuid())
-            return False
+        self._db_instance.execute(INSERT_LIGHT_STRIP_ORDER, [0, plant.uuid])
+        return False
