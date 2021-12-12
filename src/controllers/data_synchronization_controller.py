b0146edf8cfc092bf5eef259adb85ae74dc3f726
Xavier Chrétien <chretienxavier42@gmail.com>
2021-12-12 12:39:23 -0500

    WIP

--- src/controllers/data_synchronization_controller.py
+++ src/controllers/data_synchronization_controller.py
@@ -211,7 +211,6 @@ class DataSynchronizationController:
         self._db_service.execute(DELETE_SENSORS_TRANSMITTED)
         self._db_service.execute(DELETE_ACTUATORS_TRANSMITTED)
         if len(uuids) > 0:
-            self._logger.info(DELETE_PLANT_NOT_IN_UUID_LIST.replace("?", '","'.join(uuids)))
             self._db_service.execute(DELETE_PLANT_NOT_IN_UUID_LIST.replace("?", '","'.join(uuids)))
 
         self._logger.info("Gathering plants data from the API was successfully.")
